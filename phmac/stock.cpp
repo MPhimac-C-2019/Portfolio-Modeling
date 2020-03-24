1c333b5e7ae583d7f9a1ef6d4b6ef51040617aa3
Sunny Dhaliwal <sunnydhaliwal@sunnys-mbp-7.gateway.sasktel.net>
2020-03-23 19:57:08 -0600

    Building Stock Process

--- phmac/stock.cpp
+++ phmac/stock.cpp
@@ -1,7 +1,24 @@
 #include "stock.h"
 namespace phmac{
+  Stock::Stock(double mu, double vol): Instrument(){
+
+  }
+
+  Stock::~Stock(): ~Instrument(){
 
 
+  }
+  Stock::getvol(){
+
+  }
+  Stock::getmu(){
+
+  }
+
+  std::vector<double> Stock::getNormPath(double T, double t, int N){
+
+
+  }
 
 
 }
