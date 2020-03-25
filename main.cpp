3d7778db5015e8b2ada04a5b9b82040846d5fa9b
Sunny Dhaliwal <sunnydhaliwal@sunnys-mbp-7.gateway.sasktel.net>
2020-03-24 21:28:36 -0600

    Timing for sim

--- main.cpp
+++ main.cpp
@@ -8,10 +8,18 @@ int main(){
   prm::Stock *appl;
   appl = new prm::Stock(.60, .02);
 
+  unsigned tic = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+
   prm::eurocall option1(5,1,appl);
   std::cout << option1.eurocallMCPrice(5,.02,0,100000) << "\n";
 
-  std::cout << "Done" <<"\n";
+
+
+  unsigned toc = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+  std::cout << "Done" <<"\n" << (toc - tic)/1e+9 << " s Elapsed" << "\n";
+
+
   delete appl;
   appl = nullptr;
   return 0;
